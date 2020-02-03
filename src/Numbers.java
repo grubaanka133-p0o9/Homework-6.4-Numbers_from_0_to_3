c0bed34105dcea32319f3fbf4053812c127abe2b
Anna Biga <aniabiga040896@gmail.com>
2020-02-04 00:43:36 +0100

    displaying numbers

---
+++ src/Numbers.java
@@ -0,0 +1,15 @@
+public class Numbers {
+    public static void main(String[] args) {
+        double a = 0;
+        while (a<=30){
+            System.out.print(a/10 +", ");
+            a++;
+        }
+        do {
+            System.out.print(a/10 +", ");
+            a++;
+        }while (a<=30);
+
+
+    }
+}
